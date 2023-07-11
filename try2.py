b6b467b5ffdea4d033412284bda7fdf589639344
YudhaDev <yudha.dev96@gmail.com>
2023-07-11 15:22:07 +0700

    numpy implement

--- try2.py
+++ try2.py
@@ -1,20 +1,60 @@
-from tkinter import *
+import cv2
+import win32
+import win32com.client
+import numpy as np
 
-root = Tk()
+class MediaClass:
+    kernel = np.one
+    def loadImage(self):
+        img = cv2.imread("Resources/builder1.jpg")
+        cv2.imshow("Output", img)
+        cv2.waitKey(0)
+        return
 
-# variable is stored in the root object
-root.counter = 0
+    def loadImagePostFX(self, mode):
+        img = cv2.imread("Resources/builder1.jpg")
+
+        imgGreyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(imgGreyscale, (7, 7), 0)
+        imgCanny = cv2.Canny(img,200,200)
+        imgdialation =
+
+        cv2.imshow("imgGreyscale", imgGreyscale)
+        cv2.imshow("imgBlur", imgBlur)
+        cv2.imshow("imgCanny", imgCanny)
+        cv2.waitKey(0)
+
+        match mode:
+            case 0:
+                return
+            case 1:
+                return
+            case 2:
+                return
+
+    def loadVideo(self):
+        vid = cv2.VideoCapture("Resources/nature.mp4")
+        while True:
+            success, img = vid.read()
+            cv2.imshow("Output Video", img)
+            if cv2.waitKey(1) and 0xFF == ord("q"):
+                return
+
+    def loadWebcam(self):
+        cam = cv2.VideoCapture(0)
+        cam.set(3, 640)
+        cam.set(4, 480)
+
+        while True:
+            success, img = cam.read()
+            cv2.imshow("Ouput Webcam", img)
+            if cv2.waitKey(1) and 0xFF == ord("q"):
+                return
 
 
-def clicked():
-    root.counter += 1
-    L['text'] = 'Button clicked: ' + str(root.counter)
+# wmi = win32com.client.GetObject("winmgmts:")
+# for usb in wmi.InstancesOf("Win32_USBHub"):
+#     print(usb.DeviceID)
 
-
-b = Button(root, text="Click Me", command=clicked)
-b.pack()
-
-L = Label(root, text="No clicks yet.")
-L.pack()
-
-root.mainloop()
+media_object = MediaClass()
+media_object.loadImagePostFX(0)
