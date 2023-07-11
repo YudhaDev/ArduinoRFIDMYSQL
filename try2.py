be1bc1d8040acbf5a8b02350978fb20bf17cf293
YudhaDev <yudha.dev96@gmail.com>
2023-07-11 15:58:24 +0700

    progress opencv

--- try2.py
+++ try2.py
@@ -4,7 +4,7 @@ import win32com.client
 import numpy as np
 
 class MediaClass:
-    kernel = np.one
+    __kernel = np.ones((5,5), np.uint8) #sebesar matrix 5 x 5 dengan nilai unsigned int (0-255)
     def loadImage(self):
         img = cv2.imread("Resources/builder1.jpg")
         cv2.imshow("Output", img)
@@ -17,11 +17,14 @@ class MediaClass:
         imgGreyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         imgBlur = cv2.GaussianBlur(imgGreyscale, (7, 7), 0)
         imgCanny = cv2.Canny(img,200,200)
-        imgdialation =
+        imgDialation = cv2.dilate(imgCanny, self.__kernel, iterations= 1)
+        imgEroded = cv2.erode(imgDialation, self.__kernel, iterations= 1)
 
         cv2.imshow("imgGreyscale", imgGreyscale)
         cv2.imshow("imgBlur", imgBlur)
         cv2.imshow("imgCanny", imgCanny)
+        cv2.imshow("Dilation", imgDialation)
+        cv2.imshow("imgEroded", imgEroded)
         cv2.waitKey(0)
 
         match mode:
@@ -31,6 +34,23 @@ class MediaClass:
                 return
             case 2:
                 return
+
+    def resizeAndCrop(self):
+        img = cv2.imread("Resources/builder1.jpg")
+
+        imgResize = cv2.resize(img, (400,400))
+        cv2.imshow("Resize", imgResize)
+        imgCrop = img[0:300, 0:300]
+        cv2.imshow("Crop", imgCrop)
+        cv2.waitKey(0)
+
+    def editImg(self):
+        img = np.zeros((512,512)) # pixel 512 x 512 grescale
+        img = np.zeros((512,512,3),np.uint8) # pixel 512 x 512 3 channel RGB/BGR
+        img[:] = 230,10,234 # memberikan warna pada seluruh pixel
+        cv2.imshow("edit image", img)
+        cv2.waitKey(0)
+
 
     def loadVideo(self):
         vid = cv2.VideoCapture("Resources/nature.mp4")
@@ -57,4 +77,4 @@ class MediaClass:
 #     print(usb.DeviceID)
 
 media_object = MediaClass()
-media_object.loadImagePostFX(0)
+media_object.editImg()
